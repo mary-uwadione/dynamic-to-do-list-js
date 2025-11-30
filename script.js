6b78b573086c42162975db9b03cedc728f3be315
Luxar <237206419+mary-uwadione@users.noreply.github.com>
2025-11-30 21:51:31 +0100

    add folders

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    function addTask() {
+        let taskText = taskInput.ariaValueMax.trim();
+        if (taskText === "") {
+            alert("enter a text");
+            return;
+        }
+       
+        const li = document.createElement("li");
+        li.textContent = taskText;
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove";
+        removeBtn.classList.add("remove-btn");
+    
+    }
+})
